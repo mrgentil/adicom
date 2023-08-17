0fa605b9e722fefb560473d99409be4145d516f1
mrgentil <tshitshob@gmail.com.com>
2023-08-17 16:00:43 +0100

    fixed/votes

--- app/Http/Controllers/ADICOMController.php
+++ app/Http/Controllers/ADICOMController.php
@@ -8,6 +8,7 @@ use App\Models\AwardGalery;
 use App\Models\Galery;
 use App\Repositories\PodcastRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ADICOMController extends Controller
 {
@@ -73,16 +74,34 @@ class ADICOMController extends Controller
 
     public function vote(Request $request)
     {
+        // Vérifiez si l'utilisateur est connecté
+        if (auth()->guest()) {
+            return redirect()->route('login')->with('error', 'Veuillez vous connecter pour voter.');
+        }
+
         // Validez les données entrantes
         $request->validate([
             'candidate_id' => 'required|exists:awards,id',
         ]);
+
+        // Récupérez l'utilisateur actuellement connecté
+        $user = Auth::user();
+
+        // Vérifiez si l'utilisateur a déjà voté dans les 30 dernières minutes
+        $lastVotedAt = $user->last_voted_at;
+        if ($lastVotedAt && now()->diffInMinutes($lastVotedAt) < 30) {
+            return redirect()->back()->with('error', 'Vous avez déjà voté. Veuillez revenir dans 30 minutes.');
+        }
 
         $candidate = Award::findOrFail($request->candidate_id);
 
         // Incrémentez le nombre de votes pour le candidat
         $candidate->increment('votes');
 
+        // Mettez à jour la date et l'heure du dernier vote de l'utilisateur
+        $user->update(['last_voted_at' => now()]);
+
         return redirect()->back()->with('message', 'Vote enregistré avec succès.');
     }
+
 }
